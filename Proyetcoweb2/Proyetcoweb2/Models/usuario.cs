ca7a03b956b7ca5d5d9e36f60714e3153cd645f8
DESKTOP-0K97VU7\JONATHAN <jjgarciaa@est.utn.cr>
2017-07-12 22:42:17 -0600

    datos nuevos

---
+++ Proyetcoweb2/Proyetcoweb2/Models/usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyetcoweb2.Models
+{
+    public class usuario
+    {
+
+
+        [Display(Name = "Id")]
+        public int Empid { get; set; }
+
+        [Required(ErrorMessage = "Usuario Requerido.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Clave  Requerida.")]
+        public string Password { get; set; }
+
+        //usuario y clave
+    }
+}
